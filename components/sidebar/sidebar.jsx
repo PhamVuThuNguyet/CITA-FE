d34675c886db59f110a8c41b2dbfa2806f0537c5
htphong01 <thanhphonga4.hbt@gmail.com>
2022-11-07 17:24:31 +0700

    add new code

--- components/sidebar/sidebar.jsx
+++ components/sidebar/sidebar.jsx
@@ -4,7 +4,7 @@ import ItemLayout from "../layout/item-layout";
 export default function SideBar({timelineData}){
     const timeline = convertArrayToObject(timelineData.elements);
     return (<div>
-        <ItemLayout section_title={timeline.Home_Timeline_Title} section_content={timeline.Home_Timeline_Content}/>
+        <ItemLayout section_title={timeline.Home_Timeline_Title.value} section_content={timeline.Home_Timeline_Content.value}/>
         <ItemLayout/>
         <ItemLayout/>
     </div>);

d4c12f244c4c0a995306cc98e47a3e446b85c7eb
PhamVuThuNguyet <pvtnguyet.19it1@vku.udn.vn>
2023-01-05 14:49:23 +0700

    Update code

---
+++ components/schedule/schedule-content.jsx
@@ -0,0 +1,8 @@
+import styles from '../../styles/KeyNote.module.scss';
+import { convertArrayToObject } from '../../utils';
+
+export default function ScheduleContent({ data }) {
+    return(
+        <h1 style={{color: "red", fontSize: "5rem", fontFamily: "Futura"}}>UPDATING...</h1>
+      );
+}

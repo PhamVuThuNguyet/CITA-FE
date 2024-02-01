fcffdae7c30dddb9f2c451a37a100d7cb5ce5ee2
PhamVuThuNguyet <phamvuthunguyet.19it1vku@gmail.com>
2024-02-01 15:12:18 +0700

    Update cfp page

--- components/callforpaper/callforpaper-content.jsx
+++ components/callforpaper/callforpaper-content.jsx
@@ -3,16 +3,24 @@ import styles from '../../styles/Callforpaper.module.scss';
 import { convertArrayToObject } from '../../utils';
 
 const CallforpaperContent = ({ data }) => {
-  return (
-    <>
-      {data.map((layout) => (
-        <ItemLayout
-          section_title={layout.elements[0].value}
-          section_content={layout.elements[1].value}
-        ></ItemLayout>
-      ))}
-    </>
-  );
+  if (convertArrayToObject(data)['CallForPaper_General'].elements.length == 0) {
+    return (
+      <h1 style={{ color: 'red', fontSize: '5rem', fontFamily: 'Futura' }}>
+        UPDATING...
+      </h1>
+    );
+  } else {
+    return (
+      <>
+        {data.map((layout) => (
+          <ItemLayout
+            section_title={layout.elements[0].value}
+            section_content={layout.elements[1].value}
+          ></ItemLayout>
+        ))}
+      </>
+    );
+  }
 };
 
 export default CallforpaperContent;

314ae532cdcca2cc71159b4fbea84231dd70c8fb
PhamVuThuNguyet <nguyet.pham.researcher@gmail.com>
2024-07-08 23:16:24 +0800

    feat: upload file for submitted papers

---
+++ components/submittedpaper/submitted-paper-content.jsx
@@ -0,0 +1,23 @@
+import { convertArrayToObject } from '../../utils';
+const SubmittedPaperContent = ({ data }) => {
+  if (convertArrayToObject(data)['Submitted-Paper_All'].elements.length == 0) {
+    return (
+      <h1 style={{ color: 'red', fontSize: '5rem', fontFamily: 'Futura' }}>
+        UPDATING...
+      </h1>
+    );
+  } else {
+    const file =
+      convertArrayToObject(data)['Submitted-Paper_All'].elements[0].file;
+    if (file) {
+      const fileURL = file.publicUrl;
+      return (
+        <>
+          <iframe src={fileURL} width="100%" height="100%"></iframe>
+        </>
+      );
+    }
+  }
+};
+
+export default SubmittedPaperContent;

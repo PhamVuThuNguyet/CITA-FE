9f914ff697a913e662d9c48e264d119b347ea4bc
PhamVuThuNguyet <nguyet.pham.researcher@gmail.com>
2024-12-18 11:19:03 +1100

    update: update accommodation info

--- components/accommodation/accommondation-content.jsx
+++ components/accommodation/accommondation-content.jsx
@@ -5,12 +5,7 @@ const AccommodationContent = () => {
   return (
     <>
       <h1 className={styles.title}>Accommodation Information</h1>
-
-      <AccommodationItemLayout
-        section_title="Wyndham Hoi An Royal Beachfront Resort & Villas"
-        section_content="Hoi An, Vietnam"
-        map_address="https://www.google.com/maps/embed/v1/place&q=ChIJX8hf7joRQjEREPPlXdd6NfI"
-      ></AccommodationItemLayout>
+      TBD
     </>
   );
 };
